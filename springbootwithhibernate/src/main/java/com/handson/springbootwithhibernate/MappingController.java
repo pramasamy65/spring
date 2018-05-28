a7c69be9ab052e15866e19db56a2712f8ff946bc
Prakash Ramasamy <prakash@Prakashs-MacBook-Air.local>
2018-05-28 10:43:41 -0700

    SpringBoot with hibernate

---
+++ springbootwithhibernate/src/main/java/com/handson/springbootwithhibernate/MappingController.java
@@ -0,0 +1,26 @@
+package com.handson.springbootwithhibernate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("customer/details")
+public class MappingController {
+
+	@Autowired
+	DetailsDaoImpl daoImpl;
+
+	@PostMapping("/account")
+	public void saveAccounts(@RequestBody Customer customerDetails) {
+		daoImpl.saveCustomerDetails(customerDetails);
+	}
+
+	@GetMapping("/account/{id}")
+	public Customer saveAccounts(@RequestParam int customerId) {
+		return daoImpl.getCustomerDetails(customerId);
+	}
+
+}
---
+++ springbootwithhibernate/src/main/resources/schema.sql
@@ -0,0 +1,2 @@
+
+create table CUSTOMER (CUSTOMER_ID int, ACCOUNT_NUMBER int, ACCOUNT_TYPE varchar, BALANCE varchar, CUST_NAME varchar, ZIP_CODE int );
