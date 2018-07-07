dd0f832753d16c0815086967fb6a53794ee77667
Prakash Ramasamy <prakash@Prakashs-MacBook-Air.local>
2018-07-07 05:57:02 -0700

    Spring MVC handson

--- springmvc/src/main/java/com/handson/springmvc/controller/MinutesController.java
+++ springmvc/src/main/java/com/handson/springmvc/controller/MinutesController.java
@@ -1,9 +1,15 @@
 package com.handson.springmvc.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.handson.springmvc.model.Activity;
 import com.handson.springmvc.model.Exercise;
 
 @Controller
@@ -19,5 +25,17 @@ public class MinutesController {
 	public String addMoreMinutes(@ModelAttribute("exercise") Exercise exercise) {
 		System.out.println("More Minutes exercised for the day " + exercise.getMinutes());
 		return "addMinutes";
+	}
+
+	@RequestMapping(value = "/activities", method = RequestMethod.GET)
+	public @ResponseBody List<Activity> findAllActivities() {
+		List<Activity> listOfActivities = new ArrayList<Activity>();
+		Activity activity1 = new Activity();
+		activity1.setDescription("Biking");
+		Activity activity2 = new Activity();
+		activity2.setDescription("Run");
+		listOfActivities.add(activity1);
+		listOfActivities.add(activity2);
+		return listOfActivities;
 	}
 }
---
+++ springmvc/src/main/java/com/handson/springmvc/model/Activity.java
@@ -0,0 +1,15 @@
+package com.handson.springmvc.model;
+
+public class Activity {
+
+	private String description;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
